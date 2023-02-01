ca8a10546655e213e41d38513377445c4fcda6d4
cliff-rosen-rt <cliff.rosen@radialtheory.com>
2023-02-01 17:17:38 -0500

    step 1 cleanup

--- backend/data_processor/step_1_spider.py
+++ backend/data_processor/step_1_spider.py
@@ -11,32 +11,46 @@ TO DO
     isolate text within page worth saving
 """
 
+def link_is_good(link_url):
+    if link_url is not None \
+        and (link_url.startswith(initial_url) or not link_url.startswith("http"))\
+        and not link_url.startswith("#") \
+        and not link_url.startswith("mailto:") \
+        and not link_url.endswith(".jpg") \
+        and not link_url.endswith(".pdf") \
+        and link_url != '/' \
+        and link_url != initial_url + '/' \
+        and link_url not in visited_urls \
+        and initial_url + link_url not in visited_urls:
+        return True
+    else:
+        return False        
+
 # Define a function to make a request and spider the website recursively
 def spider(url):
+    visited_urls.add(url)
+    print(url, len(visited_urls))
     response = requests.get(url, headers={"User-Agent": "XY"})
     soup = BeautifulSoup(response.content, 'html.parser')
-    visited_urls.add(url)
 
     # Extract page text and save it to db
     page_text = ""
     for main in soup.find_all('main'):
-        page_text = page_text + main.get_text()
-    save_text(url, page_text)
+        page_text = page_text + main.get_text() + "\n\n-----\n\n"
+
+    #save_text(url, page_text)
+    write_text_to_file(url, page_text)
 
     # Extract all the links on the page
     links = soup.find_all('a')
     for link in links:
         link_url = link.get('href')
-
-        # Check if the link is a valid URL and has not been visited yet
-        if link_url is not None \
-            and link_url.startswith(initial_url) \
-            and not link_url.endswith(".jpg") \
-            and link_url not in visited_urls:
-            print(link_url)
-            spider(link_url)
+        if link_is_good(link_url):
+            if not link_url.startswith("http"):
+                link_url = initial_url + link_url
+            #spider(link_url)
         #else:
-            #print("rejected: ", link_url)
+        #    print("rejected: ", link_url)
 
 def save_text(uri, text):
     print("saving: ", uri)
@@ -45,44 +59,33 @@ def save_text(uri, text):
     #print(text)
     return    
 
-def run_spider():
-    conn = db.get_connection()
-    spider(initial_url)
-    db.close_connection(conn)
-    print("------------------------")
-    print("set: ", visited_urls)
+def write_text_to_file(uri, page_text):
+    with open(file_name, "a") as file:
+        file.writelines(uri + "\n\n")
+        file.writelines(page_text + "\n\n")
+        file.writelines(page_text.encode(encoding='ASCII',errors='ignore').decode() + "\n\n")
+        #file.writelines(re.sub('\s+', ' ', page_text) + "\n\n")
 
-def count_links(url):
-    response = requests.get(url, headers={"User-Agent": "XY"})
-    soup = BeautifulSoup(response.content, 'html.parser')
-    visited_urls.add(url)
 
-    # Extract all the links on the page
-    links = soup.find_all('a')
-    for link in links:
-        link_url = link.get('href')
-
-        # Check if the link is a valid URL and has not been visited yet
-        if link_url is not None \
-            and link_url.startswith(initial_url) \
-            and not link_url.endswith(".jpg") \
-            and not link_url.endswith(".pdf") \
-            and link_url not in visited_urls:
-            print(link_url)
-            spider(link_url)
-        #else:
-            #print("rejected: ", link_url)
-
-# configuration values
-domain_id = 2
+# configure job
+domain_id = 3
 initial_url = 'https://everplans.com'
+initial_url = 'https://www.everplans.com/health-organization'
+initial_url = 'https://www.everplans.com/topics'
+file_name = "page.txt"
 
-# process initial url
+# init
 visited_urls = set()
+conn = db.get_connection()
 
-count_links(initial_url)
-print("visited urls", visited_urls)
-print("visited url count", len(visited_urls))
+# do it
+spider(initial_url)
+
+# cleanup
+db.close_connection(conn)
+print("------------------------")
+print("set: ", visited_urls)
+
 
 """
 filter out pdfs but find another way to harvest them

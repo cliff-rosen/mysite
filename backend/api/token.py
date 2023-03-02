e20a416d1e9b1489aa913fc6c45e5c525e21c708
cliff-rosen-rt <cliff.rosen@radialtheory.com>
2023-03-02 08:08:26 -0500

    backup

--- backend/api/token.py
+++ backend/api/token.py
@@ -1,18 +1,7 @@
 from db import local_db as db
-import local_secrets as secrets
 from logger import Logger
-import jwt
-import datetime
-import bcrypt
+from utils import check_password, make_jwt, decode_token
 
-def make_jwt(user_id, username):
-    payload = {
-    'user_id': user_id,
-    'username': username,
-    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)
-    }
-    token = jwt.encode(payload, secrets.JWT_SECRET, algorithm='HS256')
-    return token
 
 def get_token(username, password):
     # get user record
@@ -28,13 +17,13 @@ def get_token(username, password):
     
     # verify and return
     hashed_password = res['hashed_password']
-    #hashed_password = bcrypt.hashpw(res['password'].encode('utf-8'), bcrypt.gensalt())
-    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+    if check_password(password, hashed_password):
         jwt = make_jwt(res['user_id'], res['user_name'])
         return({"status":"SUCCESS", "token": jwt})
     else:
         print('get_token error - invalid password')
         return({"status": "INVALID_LOGIN"})
+
 
 """
     if 'error' in res:
--- backend/utils.py
+++ backend/utils.py
@@ -1,9 +1,39 @@
 import secrets
 import string
-
+import jwt
+import datetime
+import bcrypt
+import local_secrets
 
 def make_new_conversation_id():
     alphabet = string.ascii_letters + string.digits  
     password = ''.join(secrets.choice(alphabet) for i in range(20))
     return password
+
+def encrypt_password(password):
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    return hashed_password
+
+def check_password(password, hashed_password):
+    if bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8')):
+        return True
+    return False
+
+def make_jwt(user_id, username):
+    payload = {
+    'user_id': user_id,
+    'username': username,
+    'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7)
+    }
+    token = jwt.encode(payload, local_secrets.JWT_SECRET, algorithm='HS256')
+    return token
+
+def decode_token(jwt_token):
+    try:
+        decoded_token = jwt.decode(jwt_token, local_secrets.JWT_SECRET, algorithms=["HS256"])
+    except Exception as e:
+        print('decode_token_error', e)
+        return {'error', str(e)}
+    return(decoded_token)
+
 
